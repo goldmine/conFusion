c7e428da6c0873b351dbe8f7ad7b698cc4474907
Gary <goldmine@163.com>
2017-11-05 13:47:44 +0800

    header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,18 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
